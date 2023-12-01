b4114dcd883e34486237326c73470a3219e7ca7b
Arnau <arnauapps@gmail.com>
2023-12-01 20:16:10 +0100

    User register form validation and request work

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'name' => ['required', 'min:3', 'max:15'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:8', 'max:30']
+        ]);
+        return "Hello from the controller";
+    }
+}
